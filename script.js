0f417b62073811fe124db582f6ad1e08b64f5eb1
mbonyimfura <mbonyimfuratresor@gmail.com>
2023-01-05 15:57:17 +0200

    third commit

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
 const button=document.querySelector('.button')
 const popup=document.querySelector('.popup-wrapper')
 const close=document.querySelector('.popup-close')
-button.addEventListener('mouseover',()=>{
+button.addEventListener('click',()=>{
 popup.style.display='block'
 })
 close.addEventListener('click',()=>{
